a2d7c20b36898260891c368759b3bcd9b536e8b7
Shinwoo PARK <devpysweb@gmail.com>
2024-10-10 21:40:35 +0900

    Update main.py

--- main.py
+++ main.py
@@ -243,12 +243,14 @@ def make_title(dawn: str | None, dusk: str | None, /):
         logger.error("Cannot find start/end date\n")
         sys.exit(1)
     m = r'^(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2})(\.\d+)?(([+-]\d{2}:\d{2})|Z)?$'
-    api_dfm, msg_dfm = "%Y-%m-%dT%H:%M:%S%z", "%d %B %Y"
+    api_dfm_dawn, api_dfm_dusk, msg_dfm = "%Y-%m-%dT%H:%M:%S%z", "%Y-%m-%dT%H:%M:%S%z", "%d %B %Y"
     if re.match(m, dawn).group(2):
         api_dfm = "%Y-%m-%dT%H:%M:%S.%f%z"
+    if re.match(m, dusk).group(2):
+        api_dfm = "%Y-%m-%dT%H:%M:%S.%f%z"
     try:
-        start_date = datetime.strptime(dawn, api_dfm).strftime(msg_dfm)
-        end_date = datetime.strptime(dusk, api_dfm).strftime(msg_dfm)
+        start_date = datetime.strptime(dawn, api_dfm_dawn).strftime(msg_dfm)
+        end_date = datetime.strptime(dusk, api_dfm_dusk).strftime(msg_dfm)
     except ValueError as err:
         logger.error(f"{err}\n")
         sys.exit(1)
